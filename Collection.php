0e3d51752805ce4dc9c032fcba299d7ed2211f3e
Damijan Cavar <damijan@cavar.si>
2018-10-09 15:26:10 +0200

    Create Collection.php

---
+++ Collection.php
@@ -0,0 +1,83 @@
+<?php
+declare(strict_types = 1);
+
+namespace Pyz\Shared\Collection;
+
+use ArrayAccess;
+use ArrayIterator;
+use IteratorAggregate;
+
+class Collection implements ArrayAccess, IteratorAggregate
+{
+
+    /**
+     * @var array
+     */
+    protected $collectionItems = [];
+
+    /**
+     * Whether a offset exists
+     *
+     * @link http://php.net/manual/en/arrayaccess.offsetexists.php
+     *
+     * @param mixed $offset
+     *
+     * @return boolean true on success or false on failure.
+     */
+    public function offsetExists($offset): bool
+    {
+        return isset($this->collectionItems[$offset]);
+    }
+
+    /**
+     * @param mixed $offset
+     *
+     * @return mixed
+     */
+    public function offsetGet($offset)
+    {
+        return $this->collectionItems[$offset];
+    }
+
+    /**
+     * Offset to set
+     *
+     * @link http://php.net/manual/en/arrayaccess.offsetset.php
+     *
+     * @param mixed $offset
+     * @param mixed $value
+     *
+     * @return void
+     */
+    public function offsetSet($offset, $value)
+    {
+        $this->collectionItems[$offset] = $value;
+    }
+
+    /**
+     * Offset to unset
+     *
+     * @link http://php.net/manual/en/arrayaccess.offsetunset.php
+     *
+     * @param mixed $offset
+     *
+     * @return void
+     */
+    public function offsetUnset($offset)
+    {
+        unset($this->collectionItems[$offset]);
+    }
+
+    /**
+     * Retrieve an external iterator
+     *
+     * @link http://php.net/manual/en/iteratoraggregate.getiterator.php
+     *
+     * @return \Traversable
+     */
+    public function getIterator()
+    {
+        return new ArrayIterator($this->collectionItems);
+    }
+
+}
